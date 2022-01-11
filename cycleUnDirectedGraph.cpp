547ecc0728b01f9619e82e3b9f8009511d459ccb
rahulsid55 <90174294+rahulsid55@users.noreply.github.com>
2022-01-11 13:03:05 +0530

    Create cycleUnDirectedGraph.cpp

---
+++ cycleUnDirectedGraph.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+  bool dfs(int node,int par,vector<int> &vis,vector<int> adj[]){
+      vis[node]=1;
+      for(auto i: adj[node]){
+          if(!vis[i]){
+              if(dfs(i,node,vis,adj))
+              return true;
+          }
+          else if(i!=par) return true;
+      }
+      return false;
+  }
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(int V, vector<int> adj[]) {
+        vector<int> vis(V,0);
+        for(int i=0;i<V;i++){
+            if(!vis[i])
+             if(dfs(i,-1,vis,adj)) return true;
+        }
+        return false;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        bool ans = obj.isCycle(V, adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
+    }
+    return 0;
+}  // } Driver Code Ends

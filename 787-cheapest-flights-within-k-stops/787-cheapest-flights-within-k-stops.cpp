cbbd5502fc9bdfffb00e8cb7d50502c6835be176
rahulsid55 <90174294+rahulsid55@users.noreply.github.com>
2022-01-20 15:55:03 +0530

    Time: 88 ms (24.47%), Space: 21 MB (19.23%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -1,45 +1,33 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        // Create Graph
-        map<int, vector<pair<int, int>>> graph;
-        
-        for (vector<int> flight : flights) {
-            int u = flight[0];
-            int v = flight[1];
-            int p = flight[2];
-            
-            if (graph.find(u) == graph.end()) graph[u] = vector<pair<int, int>>();
-            
-            graph[u].push_back({v, p});
-        }
-        
-        // { cost, node, stops }
-        priority_queue< tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>> > pq;
-        vector<int> discovery(n + 1, INT_MAX); // To Avoid TLE
-        
-        pq.push({0, src, 0});
-        
-        while (!pq.empty()) {
-            auto top = pq.top();
-            pq.pop();
-            
-            auto [cost, curr, stops] = top;
-            
-            if(stops > discovery[curr]) continue;  // To Avoid TLE
-		    discovery[curr] = stops;
-            
-            if (curr == dst) return cost;
-            
-            if (stops > k) continue;
-            
-            for (auto it : graph[curr]) {
-                auto [n, c] = it;
-                
-                pq.push({cost + c, n, stops + 1});
+    int dj(int n,vector<vector<pair<int,int>>> g,int src,int des,int k){
+        set<pair<pair<int,int>,int>> ds;
+        ds.insert({{0,src},0});
+        while(!ds.empty()){
+            auto it=ds.begin();
+            int cost=it->first.first;
+            int node=it->first.second;
+            int lvl=it->second;
+            ds.erase(it);
+            if(lvl>k+1){
+                continue;
+            }
+            if(node==des) return cost;
+            for(auto [v,d] : g[node])
+            {
+                if(lvl<=k){
+                    ds.insert({{cost+d,v},lvl+1});
+                }
             }
         }
-        
-        return -1;
+                  return INT_MAX;
+    }
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int,int>>> g(n);
+        for(auto edge: flights){
+            g[edge[0]].push_back({edge[1],edge[2]});
+        }
+        int ans=dj(n,g,src,dst,k);
+        return ans==INT_MAX?-1:ans;
     }
 };

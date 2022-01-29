5d867b517bd001e5c78e71a103f7fa1c521741c4
rahulsid55 <90174294+rahulsid55@users.noreply.github.com>
2022-01-29 11:39:31 +0530

    Time: 51 ms (40.23%), Space: 25.7 MB (8.91%) - LeetHub

--- 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.cpp
+++ 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
-    vector<int> executeInstructions(int n, vector<int>& startPos, string s) {
-        map<char,pair<int,int>> mp;
-        mp['R']={0,1};
-        mp['L']={0,-1};
-        mp['U']={-1,0};
-        mp['D']={1,0};
-        vector<pair<int,int>> v(s.length());
-        v[0]=mp[s[0]];
-        for(int i=1;i<s.length();i++){
-            v[i]={v[i-1].first+mp[s[i]].first,v[i-1].second+mp[s[i]].second};
-        }
-        vector<int> ans(s.length());
-        int len=s.length();
-        for(int i=0;i<len;i++){
-            for(int j=i;j<len;j++){
-                pair<int,int> x;
-                if(i==0){
-                     x={v[j].first,v[j].second}; 
-                }
-                else{
-                  x={v[j].first-v[i-1].first,v[j].second-v[i-1].second};   
-                }
-                
-                x={x.first+startPos[0],x.second+startPos[1]};
-                if(x.first>=0 and x.first<n and x.second>=0 and x.second<n){
-                    ans[i]=j-i+1;
-                }
-                else{
-                    break;
-                }
+     vector<int> executeInstructions(int n, vector<int>& startPos, string s) {
+        vector<int> ans;
+        
+        for(int i = 0; i < s.size(); i++) {
+            string sub = s.substr(i, s.size());
+            int r = startPos[0], c = startPos[1];
+            int cnt = 0;
+         
+            for(int j = 0; j < sub.size(); j++) {
+                if(sub[j] == 'U') r -= 1;
+                if(sub[j] == 'R') c += 1;
+                if(sub[j] == 'D') r += 1;
+                if(sub[j] == 'L')  c -= 1;
+                if(r >= 0 && r < n && c >= 0 && c < n)  cnt++; 
+                else break;      
             }
+        ans.push_back(cnt);   
         }
         return ans;
     }

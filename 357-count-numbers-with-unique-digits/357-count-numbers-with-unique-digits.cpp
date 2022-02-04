4778d65e3af41a5321602b48b8ae79cc091d3bfe
rahulsid55 <90174294+rahulsid55@users.noreply.github.com>
2022-02-04 15:41:02 +0530

    Time: 4 ms (23.87%), Space: 5.9 MB (80.11%) - LeetHub

---
+++ 357-count-numbers-with-unique-digits/357-count-numbers-with-unique-digits.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        int dp[n+1];
+        if(n==0) return 1;
+        dp[1]=10;
+        for(int i=2;i<=n;i++){
+            int ans=1;
+            int x=9;
+            for(int j=1;j<=i;j++){
+                if(j==1){
+                    ans=ans*x;
+                }
+                else if(j==2){
+                    ans=ans*x;
+                    x--;
+                }
+                else{
+                    ans=ans*x;
+                    x--;
+                }
+            }
+            dp[i]=ans+dp[i-1];
+        }
+        return dp[n];
+    }
+};

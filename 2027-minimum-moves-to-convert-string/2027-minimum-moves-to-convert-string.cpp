9063b396189f84da669262074bc1bcbda61e6ece
rahulsid55 <90174294+rahulsid55@users.noreply.github.com>
2022-02-15 16:27:28 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (38.76%) - LeetHub

--- 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.cpp
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.cpp
@@ -4,14 +4,13 @@ public:
         int i=0,n=s.length();
         int j=2;
         int count=0;
-        for(int i=0;i<n;i++){
-            cout<<i;
+        for(int i=0;i<n;){
             if(s[i]=='O'){
-                continue;
+                i++;
             }
             else{
                 count+=1;
-                i=i+2;
+                i=i+3;
                 
             }
         }

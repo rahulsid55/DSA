9e657d7bd4dcc4329aaa58655b20cf23bcf77ee4
rahulsid55 <90174294+rahulsid55@users.noreply.github.com>
2022-07-06 16:26:25 +0530

    Time: 144 ms (6.88%), Space: 352.5 MB (5.05%) - LeetHub

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    vector<vector<vector<int>>> dp;
+    int recursion(vector<int> prices,int pos,bool cool,bool bought)
+    {
+        
+        if(prices.size()<=pos) return 0;
+        if(dp[bought][cool][pos]!=-1) return dp[bought][cool][pos];
+        if(bought)
+        {
+dp[bought][cool][pos]=max(prices[pos]+recursion(prices,pos+1,true,false),recursion(prices,pos+1,cool,bought));
+        }
+        else
+        {
+            if(cool)
+            {
+                dp[bought][cool][pos]=recursion(prices,pos+1,false,bought);
+            }
+            else
+            {
+dp[bought][cool][pos]=max(-1*prices[pos]+recursion(prices,pos+1,cool,!bought),recursion(prices,pos+1,cool,bought));
+            }
+        }
+        
+        return dp[bought][cool][pos];
+    }
+    
+public:
+    int maxProfit(vector<int>& prices) {
+        dp.resize(2,vector<vector<int>>(2,vector<int>(prices.size(),-1)));
+        return recursion(prices,0,false,false);
+    }
+};

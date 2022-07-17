09fdf7414edccdaad01943438a62debe8493e4a2
rahulsid55 <90174294+rahulsid55@users.noreply.github.com>
2022-07-17 15:39:49 +0530

    Time: 9 ms (74.80%), Space: 9 MB (46.05%) - LeetHub

---
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int parent[10001];
+    int rank[10001];
+    int findPar(int node)
+    {
+        if(node==parent[node])
+            return node;
+        return parent[node]=findPar(parent[node]);
+    }
+    void union_of(int u, int v)
+    {
+        u=findPar(u);
+        v=findPar(v);
+        if(rank[u]<rank[v])
+        {
+            parent[u]=v;
+        }
+        else if(rank[u]>rank[v])
+            parent[v]=u;
+        else 
+        {
+            parent[v]=u;
+            rank[u]++;
+        }
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        vector<int> ans;
+        int n=edges.size();
+        for(int i=1;i<=n;i++){
+            parent[i]=i;
+            rank[i]=0;
+        }
+        for(auto it: edges){
+            if(findPar(it[0])==findPar(it[1])){
+                ans=it;
+            }
+            else{
+                union_of(it[0],it[1]);
+            }
+        }
+        for(int i=1;i<=n;i++) cout<<parent[i]<<" ";
+        return ans;
+        
+    }
+};

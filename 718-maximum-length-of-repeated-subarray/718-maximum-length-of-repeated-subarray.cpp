cdb8b82764db9a6398b98211e8b7010092fd205f
rahulsid55 <90174294+rahulsid55@users.noreply.github.com>
2022-07-20 02:31:55 +0530

    Time: 45 ms (99.64%), Space: 16.6 MB (57.71%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,19 +1,46 @@
 class Solution {
 public:
+    long long base = 101 , mod = 1011001110001111;
     int findLength(vector<int>& nums1, vector<int>& nums2) {
-        int n1=nums1.size(),n2=nums2.size();
-        int dp[n1+1][n2+1];
-        memset(dp,0,sizeof(dp));
-        dp[0][0]=0;
-        int maxi=0;
-        for(int i=1;i<=n1;i++){
-            for(int j=1;j<=n2;j++){
-                if(nums1[i-1]==nums2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
+         int L = 0 , R = max(nums1.size(),nums2.size()) , ans = 0;
+         while(L<=R) {
+              int m = (L+R)/2;
+              if(good(nums1,nums2,m)) {
+                    ans = m;
+                    L = m + 1;
+              }else {
+                    R = m - 1; 
+              }
+         }
+        return ans;
+    }
+    bool good(vector<int> &a,vector<int>&b,int m) {
+         long long hash = 0 , power = 1;
+         unordered_set<long long> ss;
+         int l = 0;
+         for(int i=0; i<a.size(); i++) {
+             hash = ((hash*base)%mod + a[i])%mod;
+                if(i>=m-1) {
+                    ss.insert(hash);
+                    hash = (hash - (a[l]*power)%mod)%mod;
+                    if(hash<0) hash+=mod;
+                    l++;
+                }else {
+                    power = (power * base) % mod;
                 }
-                maxi=max(maxi,dp[i][j]);
-            }
-        }
-        return maxi;
+         }
+         hash = 0 , l = 0;  
+         for(int i=0; i<b.size(); i++) {
+               hash = ((hash * base)%mod + b[i])%mod;
+                if(i>=m-1) {
+                     if(ss.find(hash)!=ss.end()) {
+                         return true;
+                     }
+                    hash = (hash - (b[l]*power)%mod)%mod;
+                    if(hash<0) hash+=mod;
+                    l++;
+                }
+         }
+        return false;
     }
 };
